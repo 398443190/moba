efba43ecef8a4abba5ed6056a589c2e2f1c771a7
鹭 黄 <389008524@qq.com>
2020-09-13 12:30:28 +0800

    修改英雄模型

--- server/models/Hero.js
+++ server/models/Hero.js
@@ -1,6 +1,29 @@
 const mongoose = require('mongoose')
 const schema = new mongoose.Schema({
     name: { type: String },
-    avatar: { type: String}
+    avatar: { type: String},
+    title: { type: String },
+    category: [{type: mongoose.SchemaTypes.ObjectId, ref: 'Category'}],
+    scories: {
+        difficult: {type: Number},
+        skill: {type: Number},
+        attack: {type: Number},
+        survice: {type: Number}
+    },
+    skills: [{
+        icon: { type: String },
+        name: { type: String },
+        description: { type: String },
+        tips: { type: String }
+    }],
+    itemssucc: [{type: mongoose.SchemaTypes.ObjectId, ref: 'Item'}],
+    itemdefeat: [{type: mongoose.SchemaTypes.ObjectId, ref: 'Item'}],
+    useageTips: { type: String },
+    battleTips: { type: String },
+    teamTips: { type: String },
+    partners: [{
+        hero: {type: mongoose.SchemaTypes.ObjectId, ref: 'Hero'},
+        description: { type: String }
+    }]
 })
 module.exports = mongoose.model('Hero', schema)
